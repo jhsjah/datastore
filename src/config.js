c834c78d897f06b73c5635c1aee4ed82b83d1ef4
abdul <abdulramiz007@gmail.com>
2025-02-14 09:15:49 +0530

    Initial commit with existing file

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
-export const API_BASE_URL = 'https://newsapibackend-746u.onrender.com/api';
+const API_BASE_URL = process.env.API_BASE_URL;
+
+fetch(API_BASE_URL)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
